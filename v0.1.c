e73e2384c3c0242f42e8503bab74625715ee2aa9
LeMarechalDeFer <talleyrandportable@LAPTOP-NDCDUSR8.>
2023-12-14 11:19:42 +0100

    Premier Commit

---
+++ v0.1.c
@@ -0,0 +1,253 @@
+//Modelisation d'une ruche d'abeille allant jusqu'à 60 000 abeilles
+
+// Constantes pour la reine
+#define DUREE_VIE_MAX_REINE 1825 // 5 ans en jours
+#define PONTE_MAX_JOUR 2000 // Nombre maximal d'œufs pondus par jour par la reine
+
+// Constantes pour les ouvrières et faux bourdons
+#define DUREE_VIE_MAX_OUVRIERE_ETE 45 // Durée de vie maximale en été en jours
+#define DUREE_VIE_MAX_OUVRIERE_HIVER 140 // Durée de vie maximale en hiver en jours
+#define DUREE_VIE_MAX_FAUX_BOURDON 90 // Durée de vie maximale en jours
+
+// Constantes pour la cellule de couvain
+#define CAPACITE_MAX_OEUFS_CELLULE 10 // Capacité maximale d'œufs par cellule de couvain
+#define CAPACITE_MAX_LARVES_CELLULE 10
+#define CAPACITE_MAX_NYMPHES_CELLULE 10
+
+// Constantes pour la cellule de stockage
+#define CAPACITE_MAX_POLLEN_CELLULE 500 // En grammes
+#define CAPACITE_MAX_MIEL_CELLULE 1000 // En grammes
+#define CAPACITE_MAX_GELEE_ROYALE_CELLULE 100 // En grammes
+#define CAPACITE_MAX_EAU_CELLULE 250 // En millilitres
+
+// Constantes pour les besoins de la ruche
+#define HUMIDITE_IDEAL 80 // Pourcentage d'humidité idéale
+#define TEMPERATURE_IDEAL 35 // Température idéale en degrés Celsius
+
+// Constantes pour les statistiques de la ruche
+#define NOMBRE_MAX_SORTIES_JOUR 10000 // Nombre maximal de sorties d'abeilles par jour
+
+// Autres constantes utiles
+#define NOMBRE_MAX_OUVRIERES 60000 // Nombre maximal d'ouvrières dans la ruche
+#define NOMBRE_MAX_FAUX_BOURDONS 2000 // Nombre maximal de faux bourdons
+
+// Ajoutez ici d'autres constantes si nécessaire
+
+
+typedef struct Reine {
+    int age; // Age en jours
+    int dureeVieMax; // Durée de vie maximale en jours
+    int ponte; // Nombre d'oeufs pondus depuis la naissance
+    int ponteMax; // Nombre d'oeufs pondus par jour
+    int sante; //  Champ pour la santé
+    int pheromones; // Niveau de phéromones, affecte le comportement des ouvrières
+} Reine;
+
+
+typedef struct Ouvriere {
+    int age; // Age en jours
+    int dureeVieMax; // Durée de vie maximale en jours
+    int sante; // Champ pour la santé
+    int efficacite; // Peut-être influencée par l'expérience
+    int experience; // Champ pour l'expérience acquise
+    RoleOuvriere role;
+} Ouvriere;
+
+
+typedef struct FauxBourdon {
+    int age; // Age en jours
+    int dureeVieMax; // Durée de vie maximale en jours
+    int enQueteReine; // Indique s'il est en quête de reproduction
+} FauxBourdon;
+
+typedef enum RoleOuvriere {
+    NETTOYEUSE,
+    NOURRICE,
+    MAGASINIERE,
+    CIRIERE,
+    VENTILEUSE,
+    GARDIENNE,
+    BUTINEUSE
+} RoleOuvriere;
+
+typedef struct Ruche {
+    Reine* reine; // Pointeur vers la reine
+    Ouvriere* ouvrieres; // Pointer vers un tableau ou une liste chaînée
+    FauxBourdon* faux_bourdons; // Pointer vers un tableau ou une liste chaînée
+    CelluleCouvain* cellulesCouvain; // Ajout de la structure des cellules de couvain
+    CelluleStockage* cellulesStockage; // Ajout de la structure des cellules de stockage
+    BesoinsRuche besoins; // Besoins en humidité et température
+    EntreeRuche entree; // Nombre d'abeilles entrant et sortant
+    SystemeVentilation ventilation; // Nombre d'abeilles ventilant la ruche
+    ZoneRepos repos; // Nombre d'abeilles pouvant se reposer
+    Statistiques stats; // Statistiques sur la ruche
+
+} Ruche;
+
+typedef struct CelluleCouvain {
+    int oeufs; // Nombre d'œufs
+    int larves; // Nombre de larves
+    int nymphes; // Nombre de nymphes
+    int nombreOeufsTotal; // Nombre total d'œufs pondus
+
+} CelluleCouvain;
+
+typedef struct CelluleStockage {
+    int capaciteNourritureTotal; // Capacité de stockage de la ruche
+    int capacitePollen; // Capacité de stockage du pollen
+    int capaciteMiel; // Capacité de stockage du miel
+    int capaciteGeléeRoyale; // Capacité de stockage de la gelée royale
+    int capaciteEau; // Capacité de stockage de l'eau
+    int pollenActuel; // Quantité de pollen stockée
+    int mielActuel; // Quantité de miel stockée
+    int geléeRoyaleActuelle; // Quantité de gelée royale stockée
+    int eauActuel; // Quantité d'eau stockée
+
+} CelluleStockage;
+
+typedef struct BesoinsRuche {
+    float humidite; // Pourcentage d'humidité
+    float temperature; // Température en degrés Celsius
+    float tauxRenouvellementAir; // Taux de renouvellement de l'air
+    float humiditeSeuil; // Seuil d'humidité à ne pas dépasser
+    float temperatureSeuil; // Seuil de température à ne pas dépasser
+
+} BesoinsRuche;
+
+typedef struct EntreeRuche {
+    int garde; // Nombre d'abeilles gardiennes
+    float fluxAbeilles; // Taux de sortie et d'entrée des abeilles
+    int nombreAbeillesSorties; // Statistiques sur les sorties des butineuses
+} EntreeRuche;
+
+typedef struct SystemeVentilation {
+    int ventilateurs; // Nombre d'abeilles ventilatrices
+    float efficaciteVentilation; // Efficacité de la ventilation (influence l'humidité et la température)
+} SystemeVentilation;
+
+typedef struct ZoneRepos {
+    int capaciteRepos; // Nombre d'abeilles pouvant se reposer en même temps
+    int abeillesRepos; // Nombre d'abeilles se reposant actuellement
+} ZoneRepos;
+
+typedef struct Statistiques {
+    int nombreOeufsPondus; // Nombre total d'œufs pondus
+    int nombreAbeillesSorties; // Nombre total d'abeilles sorties
+    int nombreAbeillesMortes; // Nombre total d'abeilles mortes
+    int nombreAbeillesNees; // Nombre total d'abeilles nées
+    int nombreAbeillesButineuses; // Nombre total d'abeilles butineuses
+    int nombreAbeillesGardiennes; // Nombre total d'abeilles gardiennes
+    int nombreAbeillesVentilatrices; // Nombre total d'abeilles ventilatrices
+    int nombreAbeillesCirieres; // Nombre total d'abeilles cirieres
+    int nombreAbeillesNourrices; // Nombre total d'abeilles nourrices
+    int nombreAbeillesNettoyeuses; // Nombre total d'abeilles nettoyeuses
+    int nombreAbeillesMagasiniere; // Nombre total d'abeilles magasiniere
+    int nombreAbeillesFauxBourdons; // Nombre total de faux bourdons
+    
+
+    int niveauMenace; // Indicateur de menaces potentielles (prédateurs, maladies)
+    int niveauSanteGlobale; // Santé globale de la ruche
+    
+} Statistiques;
+
+void mettreAJourRole(Ouvriere* ouvriere) {
+    if (ouvriere->age >= 1 && ouvriere->age <= 3) {
+        ouvriere->role = NETTOYEUSE;
+    } else if (ouvriere->age <= 9) {
+        ouvriere->role = NOURRICE;
+    } else if (ouvriere->age <= 12) {
+        ouvriere->role = MAGASINIERE;
+    } else if (ouvriere->age <= 18) {
+        ouvriere->role = CIRIERE;
+    } else if (ouvriere->age <= 21) {
+        ouvriere->role = VENTILEUSE;
+    } else if (ouvriere->age <= 25) {
+        ouvriere->role = GARDIENNE;
+    } else {
+        ouvriere->role = BUTINEUSE;
+    }
+}
+
+
+//Relatif au comportement des ouvrières 
+
+void effectuerTache(Ouvriere* ouvriere) {
+    switch (ouvriere->role) {
+        case NETTOYEUSE:
+            // Code pour le nettoyage de la ruche
+            // Par exemple, nettoyer les cellules, éjecter les déchets, etc.
+            break;
+        case NOURRICE:
+            // Code pour nourrir les larves
+            // Par exemple, préparer la gelée royale, nourrir la reine, etc.
+            break;
+        case MAGASINIERE:
+            // Code pour gérer le stockage du pollen et du miel
+            break;
+        case CIRIERE:
+            // Code pour la construction et la réparation de la ruche
+            break;
+        case VENTILEUSE:
+            // Code pour la ventilation de la ruche
+            break;
+        case GARDIENNE:
+            // Code pour la défense de la ruche
+            break;
+        case BUTINEUSE:
+            // Code pour la collecte de nectar et de pollen
+            break;
+        default:
+            // Comportement par défaut si nécessaire
+            break;
+    }
+}
+
+void miseAJourSanteEfficacite(Ouvriere* ouvriere) {
+    // Ajoutez ici la logique pour ajuster santé et efficacité
+}
+
+//Relatif à la simulation de la ruche
+
+void gererStockNourriture(Ruche* ruche) {
+    // Logique pour gérer le stock de miel et de pollen
+    // Peut-être influencé par les conditions environnementales et la santé de la ruche
+}
+
+void simulerEnvironnement(Ruche* ruche) {
+    // Mise à jour des conditions environnementales
+    // Peut influencer la disponibilité des ressources, la santé des abeilles, etc.
+}
+
+void gererEvenements(Ruche* ruche) {
+    // Logique pour gérer des événements comme des attaques de prédateurs, des maladies, etc.
+    // Ajuster le niveau de menace et prendre des mesures en conséquence
+}
+
+void evaluerSanteAbeilles(Ruche* ruche) {
+    // Évaluer la santé globale des abeilles
+    // Prendre des mesures pour traiter les maladies ou améliorer les conditions
+}
+
+
+
+void simulationJour(Ruche* ruche) {
+    for (int i = 0; i < nombreOuvrieres; i++) {
+        Ouvriere* ouvriere = &(ruche->ouvrieres[i]);
+        ouvriere->age++;
+        miseAJourSanteEfficacite(&(ruche->ouvrieres[i]));
+        mettreAJourRole(ouvriere);
+        effectuerTache(ouvriere);
+    }
+    gererStockNourriture(ruche);
+    simulerEnvironnement(ruche);
+    gererEvenements(ruche);
+
+}
+
+
+
+
+
+int main(){
+
+}
