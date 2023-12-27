4fca8e8d61855e3dd59986a60ecfb15edfd4c0d2
LeMarechalDeFer <talleyrandportable@LAPTOP-NDCDUSR8.>
2023-12-27 11:27:10 +0100

    deuxieme essaie

--- PremierEssaie/ruche.c
+++ PremierEssaie/ruche.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <SDL2/SDL.h>
 
-#define TABLE_HACHAGE_TAILLE 1000
 
 #define NOMBRE_ABEILLES_MAX 60000
 #define PONTE_MAX_JOUR 2000
@@ -120,18 +118,6 @@ typedef struct FauxBourdon {
 } FauxBourdon;
 
 
-typedef struct Noeud {
-    void* donnee; 
-    struct Noeud *suivant;
-    struct Noeud *precedent;
-} Noeud;
-
-typedef struct EntreeTableHachage {
-    int cle;   
-    Noeud* noeud;
-} EntreeTableHachage;
-
-EntreeTableHachage tableHachage[TABLE_HACHAGE_TAILLE]; 
 
 void mettreAJourRole(Ouvriere* ouvriere, enum RoleOuvriere roleOuvriere) {
     if (ouvriere->age >= 1 && ouvriere->age <= 3) {
@@ -152,61 +138,6 @@ void mettreAJourRole(Ouvriere* ouvriere, enum RoleOuvriere roleOuvriere) {
 }
 
 
-void ajouterEnTete(Noeud** tete, void* donnee) {
-    Noeud* nouveauNoeud = (Noeud*)malloc(sizeof(Noeud));
-    nouveauNoeud->donnee = donnee;
-    nouveauNoeud->suivant = (*tete);
-    nouveauNoeud->precedent = NULL;
-
-    if ((*tete) != NULL) {
-        (*tete)->precedent = nouveauNoeud;
-    }
-
-    *tete = nouveauNoeud;
-}
-
-void supprimerNoeud(Noeud** tete, Noeud* aSupprimer) {
-    if (*tete == NULL || aSupprimer == NULL) return;
-
-    if (*tete == aSupprimer) {
-        *tete = aSupprimer->suivant;
-    }
-
-    if (aSupprimer->suivant != NULL) {
-        aSupprimer->suivant->precedent = aSupprimer->precedent;
-    }
-
-    if (aSupprimer->precedent != NULL) {
-        aSupprimer->precedent->suivant = aSupprimer->suivant;
-    }
-
-    free(aSupprimer);  
-}
-
-int fonctionHachage(int cle) {
-    return cle % TABLE_HACHAGE_TAILLE;
-}
-
-void insererTableHachage(EntreeTableHachage* table, int cle, void* donnee) {
-    int index = fonctionHachage(cle);
-    ajouterEnTete(&(table[index].noeud), donnee);  
-    table[index].cle = cle;  
-}
-
-Noeud* rechercherTableHachage(EntreeTableHachage* table, int cle) {
-    int index = fonctionHachage(cle);
-    Noeud* courant = table[index].noeud;
-    while (courant != NULL) {
-        
-        // Ajouter des conditions de correspondance de clé ou de donnée ici si nécessaire
-        if (table[index].cle == cle) {
-            return courant;
-        }
-
-        courant = courant->suivant;
-    }
-    return NULL; 
-}
 
 void initialisationRuche(Ruche* ruche, unsigned int nbOuvrieres, unsigned int nbFauxBourdons){
     int i ;
@@ -246,7 +177,7 @@ void initialisationRuche(Ruche* ruche, unsigned int nbOuvrieres, unsigned int nb
         nouvelleOuvriere->experience = 0;
         nouvelleOuvriere->role = NULL;
 
-        insererTableHachage(tableHachage, cleUniqueOuvriere, nouvelleOuvriere);
+        
     }
     for(i=0; i<nbFauxBourdons; i++){
         FauxBourdon* nouveauFauxBourdon = malloc(sizeof(FauxBourdon));
@@ -255,7 +186,6 @@ void initialisationRuche(Ruche* ruche, unsigned int nbOuvrieres, unsigned int nb
             exit(EXIT_FAILURE);
         }
         //initialisation des attributs faux bourdons
-        insererTableHachage(tableHachage, cleUniqueFauxBourdon, nouveauFauxBourdon);
     }
 }
 
