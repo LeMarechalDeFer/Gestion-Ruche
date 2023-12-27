bfd0de96d9a0a1a001b17c643815ae2a1cdccf40
LeMarechalDeFer <talleyrandportable@LAPTOP-NDCDUSR8.>
2023-12-27 13:31:54 +0100

    Ajout de la generation des ouvrieres

--- abeille.h
+++ abeille.h
@@ -1,11 +1,11 @@
-#ifndef __ABEILLE_H__
-#define __ABEILLE_H__
+// #ifndef __ABEILLE_H__
+// #define __ABEILLE_H__
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <stdbool.h>
+// #include <string.h>
 
 
 
-#ifndef
+// #ifndef
