d8f1b65f303af8a66ba37e68873af59b4a2e1f51
Naokir0 <aberrada@et.esiea.fr>
2024-01-05 08:08:20 +0100

    Animation remise en route mais avec un lag qui est inférieur à celui d'avant

--- V.SDL/game.c
+++ V.SDL/game.c
@@ -22,10 +22,12 @@ SDL_Surface* image_Ruche = NULL;
 SDL_Texture* texture_ruche = NULL;
 
 SDL_Surface *BEE_surface = NULL;
-SDL_Texture *BEE_texture = NULL;
+SDL_Texture* BEE_texture[4];
+
 
 SDL_Surface* surface_Timer = NULL;
 SDL_Texture* texture_Timer = NULL;
+
 
 extern SDL_Renderer* renderer;
 extern SDL_Texture* texture_background;
@@ -58,14 +60,18 @@ Saison seasons[] = {
 
 //Ruche ruches[BEES];
 void load_bee_texture()
-{
-        SDL_Surface* BEE_surface = SDL_LoadBMP("Assets/Position_1.bmp");
+{   for(int i = 1; i<= 4;i++)
+    {
+        char filePath[50];
+        snprintf(filePath, sizeof(filePath), "Assets/Position_%d.bmp", i );
+
+        SDL_Surface* BEE_surface = SDL_LoadBMP(filePath);
         if (!BEE_surface) {
             printf("Failed to load bee frame! SDL Error: %s\n", SDL_GetError());
             exit(EXIT_FAILURE);
         }
 
-        BEE_texture = SDL_CreateTextureFromSurface(renderer, BEE_surface);
+        BEE_texture[i] = SDL_CreateTextureFromSurface(renderer, BEE_surface);
 
         if (!BEE_texture) {
             printf("Failed to create texture for bee frame! SDL Error: %s\n", SDL_GetError());
@@ -73,6 +79,7 @@ void load_bee_texture()
         }
         SDL_FreeSurface(BEE_surface);
         //SDL_FreeTexture() ; 
+    }
 }
 void change_saisons(int seasonIndex, SDL_Texture **texture_background) {
     // Assurez-vous que l'index de saison est valide
@@ -100,105 +107,6 @@ void change_saisons(int seasonIndex, SDL_Texture **texture_background) {
     }
 }
 
-/*
-void Display(float Temp_écoulé)
-{
-    TTF_Font* font = TTF_OpenFont("Config/Roboto-BlackItalic.ttf", 24);
-    SDL_SetRenderDrawColor(renderer,0,0,0,255);
-    SDL_RenderClear(renderer);
-
-    int Current_frame = 1;
-
-    if (!font) {
-        printf("Failed to load font: %s\n", TTF_GetError());
-        exit (-1);
-    }
-
-    SDL_Color color = {255, 255, 255, 255}; // White color for the font
-    SDL_RenderClear(renderer);
-    char timeText[1000];
-
-    sprintf(timeText, "Time: %.1f seconds", Temp_écoulé);
-
-    SDL_Surface *surface_background = SDL_LoadBMP("Assets/back_winter.bmp");
-    SDL_Surface *Ruche_surface = SDL_LoadBMP(HIVE_IMAGE);
-    SDL_Surface* surface_Timer = TTF_RenderText_Solid(font, timeText, color);
-
-    if(surface_background == NULL)
-    {
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_ExitWithError("Failed to load Backround image");
-    }
-
-    SDL_Texture *texture_background = SDL_CreateTextureFromSurface(renderer, surface_background);
-    SDL_Texture *Ruche_texture = SDL_CreateTextureFromSurface(renderer, Ruche_surface);
-    SDL_Texture *texture_Timer = SDL_CreateTextureFromSurface(renderer, surface_Timer);
-
-    SDL_FreeSurface(surface_background);
-    SDL_FreeSurface(Ruche_surface);
-    SDL_FreeSurface(surface_Timer);
-
-
-    if(texture_background == NULL)
-    {
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_ExitWithError("Failed to create texture");
-    }
-    SDL_Rect Background;
-    if (SDL_QueryTexture(texture_background, NULL,NULL,&Background.w, &Background.h)!= 0)
-    {
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_ExitWithError("Failed to load texture");
-    }
-    Background.x = (WINDOW_WIDTH - Background.w)/2;
-    Background.y = (WINDOW_HEIGHT - Background.h)/2;
-
-    SDL_Rect ruche_position;
-    if (SDL_QueryTexture(Ruche_texture, NULL,NULL,&ruche_position.w, &ruche_position.h)!= 0)
-    {
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_ExitWithError("Failed to load Hive texture");
-    }
-    ruche_position.x = 20;
-    ruche_position.y = 240;
-    load_bee_texture();
-
-    if(SDL_RenderCopy(renderer,texture_background,NULL,&Background))
-    {
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_ExitWithError("Failed to render Copy");
-    }
-
-    if(SDL_RenderCopy(renderer,Ruche_texture,NULL,&ruche_position))
-    {
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_ExitWithError("Failed to render Copy the Hive");
-    }
-
-    SDL_Rect timer_position = { 400, 0, 200, 50 };
-    if(SDL_RenderCopy(renderer, texture_Timer, NULL, &timer_position))
-    {
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_ExitWithError("Failed to render Copy the Timer");
-    }
-    SDL_Rect BEE_rect = { 100, 100, 50, 50 };
-    if (SDL_RenderCopy(renderer,BEE_texture,NULL,&BEE_rect))
-    {
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window); 
-        SDL_ExitWithError("Failed to render copy the BEE !");
-    }
-    SDL_RenderPresent(renderer);
-    SDL_RenderClear(renderer);
-}
-*/
 void Display(float elapsed_time)
 {
     // Charger la police et préparer le texte du timer
@@ -233,21 +141,55 @@ void Display(float elapsed_time)
     } else {
         printf("No background texture to display.\n");
     }
-
     // Dessiner le timer
     SDL_Rect timerRect = {400, 0, 200, 50}; // Position et taille du timer
     if (SDL_RenderCopy(renderer, texture_Timer, NULL, &timerRect) != 0) {
         SDL_ExitWithError("Failed to render timer texture");
     }
+    SDL_Surface *Ruche_surface = SDL_LoadBMP(HIVE_IMAGE);
+    SDL_Texture *Ruche_texture = SDL_CreateTextureFromSurface(renderer, Ruche_surface);
+    SDL_FreeSurface(Ruche_surface);
 
+    SDL_Rect ruche_position;
+    if (SDL_QueryTexture(Ruche_texture, NULL,NULL,&ruche_position.w, &ruche_position.h)!= 0)
+    {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_ExitWithError("Failed to load Hive texture");
+    }
+    ruche_position.x = 20;
+    ruche_position.y = 240;
+
+    if(SDL_RenderCopy(renderer,Ruche_texture,NULL,&ruche_position))
+    {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_ExitWithError("Failed to render Copy the Hive");
+    }
+    load_bee_texture();
+
+    SDL_Rect BEE_rect = { 100, 100, 50, 50 };
+    for(int i = 1; i<= 4;i++)
+    {
+        if(SDL_RenderCopy(renderer,BEE_texture[i],NULL,&BEE_rect))
+        {
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+            SDL_ExitWithError("Failed to render Copy the BEE");                               
+        }
+        SDL_RenderPresent(renderer);
+        SDL_Delay(200);
+    }  
     // Mettre à jour l'écran
     SDL_RenderPresent(renderer);
-
+    for(int i = 1;i<=4;i++)
+    {
+        SDL_DestroyTexture(BEE_texture[i]);
+    }
     // Libération des ressources
     SDL_DestroyTexture(texture_Timer); // Libérer la texture du timer
     TTF_CloseFont(font); // Fermer la police
 }
-
 
 SDL_Window* initSDL()
 {                         // Initialisation et création de fenêtre
